c380c00f02cf192665b192ef72d83335520108c9
Noah Thomas <noahdawg61@gmail.com>
2025-12-30 12:35:31 -0600

    Cleanup: Remove 60 temporary SQL scripts and instruction files
    These were one-time setup/debug files that shouldn't be in the repo:
    - 42 SQL migration/fix scripts
    - 17 setup instruction markdown files
    - 1 template text file
    
    Only README.md remains for documentation.

--- DIRECT_PROFILE_FIX.sql
+++
@@ -1,8 +0,0 @@
--- Direct fix for Noah's profile based on console logs
--- Update the profile for user ID: ce486dc8-967a-49a3-8fa2-20155dc372ce
-
-UPDATE public.profiles 
-SET 
-  first_name = 'Noah',
-  last_name = 'Thomas'
-WHERE id = 'ce486dc8-967a-49a3-8fa2-20155dc372ce';
